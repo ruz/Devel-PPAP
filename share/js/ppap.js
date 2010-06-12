2971dce457964e2ae1dce83be93b5de1f5731ee4
Sergei Mozhaisky <technix@voltaic.(none)>
2010-06-12 14:03:19 +0300

    - added simple style table - table sorting fixed - handling of prefix in ppap_html fixed

--- share/js/ppap.js
+++ share/js/ppap.js
@@ -1,7 +1,19 @@
 
 $(document).ready(function() 
     { 
-        $("table.sortable").tablesorter(); 
+        $("table.sortable").tablesorter( {textExtraction: ppapTextExtraction} ); 
     } 
 );
 
+var ppapTextExtraction = function(node)  
+{  
+    // extract data from markup and return it  
+   var html = node.innerHTML;
+//if (html.match(new RegExp(/([0-9\.]+?)\s+(.+?)/)))
+//{
+	
+   html = html.replace(/^([0-9\.]+?)\s+(.+?)$/, '$1');
+//	alert(html);
+//}
+   return html; 
+} 
